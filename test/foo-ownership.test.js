077833d9516d911ca6d9d37cc624a71787d66c5a
Mihai Dima <mihaizn@gmail.com>
2016-10-12 17:42:36 +0300

    linting

--- test/foo-ownership.test.js
+++ test/foo-ownership.test.js
@@ -1,52 +1,50 @@
 const Lab = require('lab')
-const assert = require('power-assert')
 const lab = exports.lab = Lab.script()
 const {describe, it, before} = lab
 const Code = require('code')
 const {expect} = Code
-const sinon = require('sinon')
 const Iam = require('../lib/index.js')
 
 describe('Ownership', () => {
-	var policies = [{
-		Version: '2106-10-17',
-		Statement: [{
-			Effect: 'Allow',
-			Action: ['foo:bar:clone',
-				'foo:bar:delete',
-				'foo:bar:list',
-				'foo:bar:read',
-				'foo:bar:publish'],
-			Resource: ['resources/${req:UserName}/*']
-		}]
-	}]
-	let iam
+  var policies = [{
+    Version: '2106-10-17',
+    Statement: [{
+      Effect: 'Allow',
+      Action: ['foo:bar:clone',
+        'foo:bar:delete',
+        'foo:bar:list',
+        'foo:bar:read',
+        'foo:bar:publish'],
+      Resource: ['resources/${req:UserName}/*']
+    }]
+  }]
+  let iam
 
-	lab.before((done) => {
-		Iam(policies, (i) => {
-			expect(i).to.exist()
+  before((done) => {
+    Iam(policies, (i) => {
+      expect(i).to.exist()
 
-			iam = i
+      iam = i
 
-			done()
-		})
-	})
+      done()
+    })
+  })
 
-	it('should allow', done => {
-		iam.process('resources/bob/something', 'foo:bar:list', {UserName: 'bob'}, (err, result) => {
-			expect(err).to.not.exist()
-			expect(result).to.be.true()
+  it('should allow', done => {
+    iam.process('resources/bob/something', 'foo:bar:list', {UserName: 'bob'}, (err, result) => {
+      expect(err).to.not.exist()
+      expect(result).to.be.true()
 
-			done()
-		})
-	})
+      done()
+    })
+  })
 
-	it('should not allow', done => {
-		iam.process('resources/fred/anotherthing', 'foo:bar:list', (err, result) => {
-			expect(err).to.not.exist()
-			expect(result).to.be.false()
+  it('should not allow', done => {
+    iam.process('resources/fred/anotherthing', 'foo:bar:list', (err, result) => {
+      expect(err).to.not.exist()
+      expect(result).to.be.false()
 
-			done()
-		})
-	})
+      done()
+    })
+  })
 })
